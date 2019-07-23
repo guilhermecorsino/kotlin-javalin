0e39a7cd12a929713cea0e2ee7efd660570cb474
Guilherme Corsino <guilhermecorsino@outlook.com>
2019-07-23 01:38:05 -0300

    formating file

--- src/main/kotlin/requests/CreateAccountRequest.kt
+++ src/main/kotlin/requests/CreateAccountRequest.kt
@@ -1,4 +1,6 @@
 package requests
 
-data class CreateAccountRequest(val id: String,
-                                val balance: Double)
+data class CreateAccountRequest(
+    val id: String,
+    val balance: Double
+)
